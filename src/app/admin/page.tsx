15805d979ed6f8d7aaf5ec74b8fc8945e883607d
Mo Amir <mo.amir.code@gmail.com>
2024-07-03 21:58:30 +0530

    router navigation error while deploying on admin page has been fixed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,24 +1,25 @@
-"use client"
-import ProjectForm from '@/components/admin/ProjectForm'
-import { useAppSelector } from '@/redux/hooks'
-import { selectIsAdminLoggedIn } from '@/redux/slices/auth/authSlice'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-
+"use client";
+import ProjectForm from "@/components/admin/ProjectForm";
+import { useAppSelector } from "@/redux/hooks";
+import { selectIsAdminLoggedIn } from "@/redux/slices/auth/authSlice";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Admin = () => {
   const navigate = useRouter();
   const isAdminLoggedIn = useAppSelector(selectIsAdminLoggedIn);
 
-  if(!isAdminLoggedIn){
-    navigate.push("/admin/auth/signin");
-  }
+  useEffect(() => {
+    if (!isAdminLoggedIn) {
+      navigate.push("/admin/auth/signin");
+    }
+  }, [isAdminLoggedIn, navigate]);
 
   return (
-    <div className='w-full' >
+    <div className="w-full">
       <ProjectForm />
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
