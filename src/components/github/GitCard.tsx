5213188f79fcc6a71a681a5bc51fd09353d044d5
Mo Amir <mo.amir.code@gmail.com>
2024-08-22 09:06:17 +0530

    OnView animation added in git card also

--- src/components/github/GitCard.tsx
+++ src/components/github/GitCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { MyContext } from "@/context/context";
+import OnView from "@/providers/OnView";
 import React, { useContext } from "react";
 import { FaRegStar } from "react-icons/fa";
 import { GoRepoForked } from "react-icons/go";
@@ -25,41 +26,51 @@ const GitCard = ({
   const { theme } = useContext(MyContext);
 
   return (
-    <div className="flex-1 min-w-[250px] hover:shadow-lg duration-150 transition-all cursor-pointer bg-secondary-color px-3 py-4">
-      <div className="flex flex-col gap-3 justify-between">
-        <h3 style={{ color: theme.bg.primary }}>{name}</h3>
-        <div className="flex items-center justify-between">
-          <div
-            style={{ color: theme.bg.primary }}
-            className="flex items-center gap-2"
-          >
-            <div className="flex items-center gap-1">
-              <RxEyeOpen size={16} />
-              <span className="text-gray-100" >{watchers}</span>
+    <OnView>
+      <div className="flex-1 min-w-[250px] hover:shadow-lg duration-150 transition-all cursor-pointer bg-secondary-color px-3 py-4">
+        <div className="flex flex-col gap-3 justify-between">
+          <h3 style={{ color: theme.bg.primary }}>{name}</h3>
+          <div className="flex items-center justify-between">
+            <div
+              style={{ color: theme.bg.primary }}
+              className="flex items-center gap-2"
+            >
+              <div className="flex items-center gap-1">
+                <RxEyeOpen size={16} />
+                <span className="text-gray-100">{watchers}</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <GoRepoForked size={16} />
+                <span className="text-gray-100">{fork}</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaRegStar size={16} />
+                <span className="text-gray-100">{stars}</span>
+              </div>
             </div>
-            <div className="flex items-center gap-1">
-              <GoRepoForked size={16} />
-              <span className="text-gray-100" >{fork}</span>
-            </div>
-            <div className="flex items-center gap-1">
-              <FaRegStar size={16} />
-              <span className="text-gray-100" >{stars}</span>
-            </div>
-          </div>
 
-          <div className="flex items-center gap-2">
-            <a style={{color: theme.bg.primary}} href={gitLink} target="_blank">
-              <PiGithubLogoLight size={16} />
-            </a>
-            {!!deployedLink && (
-              <a style={{color: theme.bg.primary}} href={deployedLink} target="_blank">
-                <HiOutlineLink size={16} />
+            <div className="flex items-center gap-2">
+              <a
+                style={{ color: theme.bg.primary }}
+                href={gitLink}
+                target="_blank"
+              >
+                <PiGithubLogoLight size={16} />
               </a>
-            )}
+              {!!deployedLink && (
+                <a
+                  style={{ color: theme.bg.primary }}
+                  href={deployedLink}
+                  target="_blank"
+                >
+                  <HiOutlineLink size={16} />
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </OnView>
   );
 };
 
